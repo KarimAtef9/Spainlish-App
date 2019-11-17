20ae328448c187e77de7525dd9008d8580ddf956
Karim <karimatef65@gmail.com>
2019-11-18 01:04:00 +0200

    view pager done (shadow line bug)

--- app/src/main/java/com/example/android/spanlish/MainActivity.java
+++ app/src/main/java/com/example/android/spanlish/MainActivity.java
@@ -1,26 +1,21 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.spanlish;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+
+import Fragments.ColorsFragment;
+import Fragments.FamilyFragment;
+import Fragments.NumbersFragment;
+import Fragments.PhrasesFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,69 +26,50 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        Intent colorsIntent = new Intent(this, ColorsActivity.class);
-        startActivity(colorsIntent);
-        // Find the View that shows the numbers category
-        TextView numbers = (TextView) findViewById(R.id.numbers);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
 
-        // Set a click listener on that View
-        numbers.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the numbers View is clicked on.
-            @Override
-            public void onClick(View view) {
-                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-                startActivity(numbersIntent);
-            }
-        });
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new NumbersFragment(), getString(R.string.category_numbers));
+        viewPagerAdapter.addFragment(new FamilyFragment(), getString(R.string.category_family));
+        viewPagerAdapter.addFragment(new ColorsFragment(), getString(R.string.category_colors));
+        viewPagerAdapter.addFragment(new PhrasesFragment(), getString(R.string.category_phrases));
 
-        TextView colors = (TextView) findViewById(R.id.colors);
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
-                startActivity(colorsIntent);
-            }
-        });
-
-        TextView family = (TextView) findViewById(R.id.family);
-        family.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-                startActivity(familyIntent);
-            }
-        });
-
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-                startActivity(phrasesIntent);
-            }
-        });
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
 
+        ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
 
-//    public void openNumbersList(View view) {
-//        Intent intent = new Intent(this, NumbersActivity.class);
-//        startActivity(intent);
-//    }
-//
-//    public void openFamilyList(View view) {
-//        Intent intent = new Intent(this, FamilyActivity.class);
-//        startActivity(intent);
-//    }
-//
-//    public void openColorsList(View view) {
-//        Intent intent = new Intent(this, ColorsActivity.class);
-//        startActivity(intent);
-//    }
-//
-//    public void openPhrasesList(View view) {
-//        Intent intent = new Intent(this, PhrasesActivity.class);
-//        startActivity(intent);
-//    }
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        void addFragment(Fragment fragment, String title) {
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+
+    }
+
 }
